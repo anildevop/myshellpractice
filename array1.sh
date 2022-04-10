267e7a2bf94dcff55ecca8115e614417e05b95ba
Ubuntu <ubuntu@ip-172-31-17-16.us-east-2.compute.internal>
2022-04-10 09:20:32 +0000

    tillarray

---
+++ array1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NAME[0]="Zara"
+NAME[1]="Qadir"
+NAME[2]="Mahnaz"
+NAME[3]="Ayan"
+NAME[4]="Daisy"
+echo "First Index: ${NAME[0]}"
+echo "Second Index: ${NAME[1]}"
+echo "all the list in array: ${NAME[*]}"
+echo "all the list in array: ${NAME[@]}"
+
---
+++ array2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+NAMES=("anil","kumar","hello")
+echo "First Index: ${NAMES[0]}"
+echo "Second Index: ${NAMES[1]}"
+echo "all the list in array: ${NAMES[*]}"
+echo "all the list in array: ${NAMES[@]}"
+
